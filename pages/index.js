0aaa6ec7759520e389c9ddd2f5cf8d7f6433ecc4
David Levai <david@davidlevai.com>
2022-04-11 11:51:45 -0500

    remove test data

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,6 @@ import Head from 'next/head';
 import { useForm } from 'react-hook-form';
 import UserTable from '../components/UserTable';
 
-const people = [
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-];
-
 export default function Home() {
   const [data, setData] = useState(null);
   const {
